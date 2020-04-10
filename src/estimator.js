1182dfeb5280781c402783c452bee903f27d39a2
olawalejuwonm <olawalejuwon@gmail.com>
2020-04-10 13:23:07 +0100

    Solution From Challenge 1 to 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,13 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {data};
+
+const covid19ImpactEstimator = (data) => {
+	const input = data;
+return{ 
+ data: input, 
+ impact: {}, 
+ severeImpact: {}
+  };
+ };
+
 
 export default covid19ImpactEstimator;
